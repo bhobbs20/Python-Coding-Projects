dbb4371d563da60d9f4d10b25483d35138c85305
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-27 10:56:39 -0500

    attempt one on txt file drill

--- os_index.py
+++ os_index.py
@@ -1,4 +1,15 @@
 import os
+import time
 
-get_current_dir = os.getcwd()
-print(get_current_dir)
+path = "/Users/pigeon/Tech_Docs/"
+files = os.listdir(path)
+
+for file in files:
+    if file.endswith('.txt'):
+        mod_time = os.path.getmtime(path)
+        local_time = time.ctime(mod_time)
+        print("{} Last modification: {}".format(file, local_time))
+    else:
+        ("Sorry there are no txt files available")
+
+
