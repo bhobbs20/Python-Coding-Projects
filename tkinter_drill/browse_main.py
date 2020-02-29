5d35f41575c39777723dd01b97dce51119f82ea3
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-28 19:19:23 -0500

    add buttons and text fields

--- tkinter_drill/browse_main.py
+++ tkinter_drill/browse_main.py
@@ -1,6 +1,6 @@
 
 
-import tkinter
+import tkinter as tk
 from tkinter import *
 
 
@@ -14,6 +14,24 @@ class ParentWindow(Frame):
         self.master.geometry("{}x{}".format(700, 400))
         self.master.title("Check Files")
         self.master.config(bg="lightgrey")
+
+        self.btn_browse_1 = tk.Button(self.master, width=12, height=1, text='Browse', bg="lightgrey", highlightbackground='lightgrey')
+        self.btn_browse_1.grid(row=0, column=1, padx=(30, 0), pady=(120, 0), sticky=E)
+
+        self.txt_browse_file_1 = tk.Entry(self.master, text='')
+        self.txt_browse_file_1.grid(row=0, column=2, rowspan=1, columnspan=9, padx=(30, 40), pady=(120, 0), sticky=N + E + W)
+
+        self.btn_browse_2 = tk.Button(self.master, width=12, height=1, text='Browse', bg="lightgrey", highlightbackground='lightgrey')
+        self.btn_browse_2.grid(row=1, column=1, padx=(30, 0), pady=(5, 0), sticky=E)
+
+        self.txt_browse_file_1 = tk.Entry(self.master, text='')
+        self.txt_browse_file_1.grid(row=1, column=2, rowspan=1, columnspan=9, padx=(30, 40), pady=(5, 0), sticky=N + E + W)
+
+        self.btn_check_files = tk.Button(self.master, width=12, height=1, text='Check Files..', bg="lightgrey", highlightbackground='lightgrey')
+        self.btn_check_files.grid(row=3, column=1, padx=(30, 0), pady=(40, 0), sticky=E)
+
+        self.btn_cancel = tk.Button(self.master, width=12, height=1, text='Close Program', bg="lightgrey", highlightbackground='lightgrey')
+        self.btn_cancel.grid(row=3, column=10, padx=(0, 30), pady=(40, 0))
 
 
 if __name__ == "__main__":
