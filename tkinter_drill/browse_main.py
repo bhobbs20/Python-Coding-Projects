96a8ec0b88adcc77014758703fe8b81720487151
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-29 06:56:17 -0500

    completed layout of window

--- tkinter_drill/browse_main.py
+++ tkinter_drill/browse_main.py
@@ -11,15 +11,16 @@ class ParentWindow(Frame):
         self.master = master
         self.master = master
         self.master.resizable(width=False, height=False)
-        self.master.geometry("{}x{}".format(700, 400))
+        # self.master.geometry("{}x{}".format(700, 400))
+        self.master.geometry()
         self.master.title("Check Files")
         self.master.config(bg="lightgrey")
 
         self.btn_browse_1 = tk.Button(self.master, width=12, height=1, text='Browse', bg="lightgrey", highlightbackground='lightgrey')
-        self.btn_browse_1.grid(row=0, column=1, padx=(30, 0), pady=(120, 0), sticky=E)
+        self.btn_browse_1.grid(row=0, column=1, padx=(30, 0), pady=(50, 0), sticky=E)
 
         self.txt_browse_file_1 = tk.Entry(self.master, text='')
-        self.txt_browse_file_1.grid(row=0, column=2, rowspan=1, columnspan=9, padx=(30, 40), pady=(120, 0), sticky=N + E + W)
+        self.txt_browse_file_1.grid(row=0, column=2, rowspan=1, columnspan=9, padx=(30, 40), pady=(50, 0), sticky=N + E + W)
 
         self.btn_browse_2 = tk.Button(self.master, width=12, height=1, text='Browse', bg="lightgrey", highlightbackground='lightgrey')
         self.btn_browse_2.grid(row=1, column=1, padx=(30, 0), pady=(5, 0), sticky=E)
@@ -28,10 +29,10 @@ class ParentWindow(Frame):
         self.txt_browse_file_1.grid(row=1, column=2, rowspan=1, columnspan=9, padx=(30, 40), pady=(5, 0), sticky=N + E + W)
 
         self.btn_check_files = tk.Button(self.master, width=12, height=1, text='Check Files..', bg="lightgrey", highlightbackground='lightgrey')
-        self.btn_check_files.grid(row=3, column=1, padx=(30, 0), pady=(40, 0), sticky=E)
+        self.btn_check_files.grid(row=3, column=1, padx=(30, 0), pady=(40, 40), sticky=E)
 
         self.btn_cancel = tk.Button(self.master, width=12, height=1, text='Close Program', bg="lightgrey", highlightbackground='lightgrey')
-        self.btn_cancel.grid(row=3, column=10, padx=(0, 30), pady=(40, 0))
+        self.btn_cancel.grid(row=3, column=10, padx=(0, 30), pady=(40, 40))
 
 
 if __name__ == "__main__":
